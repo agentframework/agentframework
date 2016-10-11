c30e31e29b2d539e85f995c0e318a782eaf3600d
e2tox <e2tox@live.com>
2016-10-11 15:22:46 +0800

    reduce memory footprint

--- src/lib/core/reflection.ts
+++ src/lib/core/reflection.ts
@@ -10,73 +10,55 @@ export class Reflection {
   private static metadata: Metadata = new Metadata();
   private _attributes: Array<IAttribute>;
   
-  constructor(private _target: Object, private _targetKey?: string | symbol, private _descriptor?: PropertyDescriptor) {
+  private constructor(private _target: Object, private _targetKey?: string | symbol, private _descriptor?: PropertyDescriptor) {
     if (IsUndefined(_descriptor) && !IsUndefined(_targetKey)) {
       this._descriptor = Object.getOwnPropertyDescriptor(_target, _targetKey);
     }
     this._attributes = [];
   }
   
-  /**
-   * Get Reflection object
-   * @param target
-   * @param targetKey
-   * @returns {Reflection}
-   */
-  public static getInstance(target: Object | Function,
-                            targetKey?: string | symbol): Reflection {
+  private static getInstance(target: Object | Function,
+                            targetKey?: string | symbol): Reflection | null {
     if (!IsObject(target)) {
       throw new TypeError();
     }
     if (!IsUndefined(targetKey)) {
       targetKey = ToPropertyKey(targetKey);
     }
-    let reflection = Reflection.metadata.get(target, targetKey);
-    if (!reflection) {
-      reflection = new Reflection(target, targetKey);
-      Reflection.metadata.saveOwn(reflection, target, targetKey);
-    }
-    return reflection;
+    return Reflection.metadata.get(target, targetKey);
   }
   
-  /**
-   * Get Reflection object from itself or parent
-   * @param target
-   * @param targetKey
-   * @param descriptor
-   * @returns {Reflection}
-   */
-  public static getOwnInstance(target: Object | Function,
-                               targetKey?: string | symbol,
-                               descriptor?: PropertyDescriptor): Reflection {
+  private static getOwnInstance(target: Object | Function,
+                               targetKey?: string | symbol): Reflection {
     if (!IsObject(target)) {
       throw new TypeError();
     }
     if (!IsUndefined(targetKey)) {
       targetKey = ToPropertyKey(targetKey);
     }
-    let reflection = Reflection.metadata.getOwn(target, targetKey);
-    if (!reflection) {
-      reflection = new Reflection(target, targetKey, descriptor);
-      Reflection.metadata.saveOwn(reflection, target, targetKey);
-    }
-    return reflection;
+    return Reflection.metadata.getOwn(target, targetKey);
   }
   
   public static getAttributes(target: Object | Function, targetKey?: any): Array<IAttribute> {
-    
-    return Reflection.getInstance(target, targetKey).getAttributes();
+    const reflection = Reflection.getInstance(target, targetKey);
+    return reflection ? reflection.getAttributes() : [];
   }
   
   public static hasAttributes(target: Object | Function, targetKey?: any): boolean {
-    return Reflection.getInstance(target, targetKey).hasAttributes();
+    const reflection = Reflection.getInstance(target, targetKey);
+    return reflection ? reflection.hasAttributes() : false;
   }
   
   public static addAttribute(attribute: IAttribute,
                              target: Object | Function,
-                             targetKey?: string | symbol, descriptor?: PropertyDescriptor) {
-    
-    Reflection.getOwnInstance(target, targetKey).addAttribute(attribute);
+                             targetKey?: string | symbol,
+                             descriptor?: PropertyDescriptor) {
+    let reflection = Reflection.getOwnInstance(target, targetKey);
+    if (reflection === null) {
+      reflection = new Reflection(target, targetKey, descriptor);
+      Reflection.metadata.saveOwn(reflection, target, targetKey);
+    }
+    reflection.addAttribute(attribute);
   }
   
   getAttributes(): Array<IAttribute> {
