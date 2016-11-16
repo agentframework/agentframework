954d34c5b07c2572d5bd2bd6aad9f7dca79ab9a5
Ling <e2tox@users.noreply.github.com>
2016-11-16 15:14:41 +0800

    coverage (#1)

--- src/lib/domain.spec.ts
+++ src/lib/domain.spec.ts
@@ -10,7 +10,7 @@ class Base {
 @agent('TestAgent2')
 class Agent extends Base {
 
-  constructor(private _domain: Domain, private _name: string) {
+  constructor(private _domain: Domain, private _name?: string) {
     super();
   }
 
@@ -34,18 +34,62 @@ describe('@domain', () => {
       expect(agent1['tested']).toBe(true);
     });
 
+    it('add agent', () => {
+      const domain1 = new Domain();
+      domain1.addAgent(Agent);
+      const a1 = domain1.getAgent('TestAgent');
+      const a2 = domain1.getAgent('TestAgent2');
+      expect(a1).toEqual(a2);
+    });
+
   });
 
-  // describe('# should not able to', () => {
-  //
-  //   it('create same agent again', () => {
-  //     const domain2 = new Domain<any>();
-  //     domain2.createAgent(Agent, 'Domain');
-  //     expect(() => {
-  //       domain2.createAgent(Agent, 'Domain');
-  //     }).toThrowError()
-  //   });
-  //
-  // });
+  describe('# should not able to', () => {
+
+    it('create same agent again', () => {
+      const domain2 = new Domain();
+      domain2.createAgent(Agent, 'Domain');
+      expect(() => {
+        domain2.createAgent(Agent, 'Domain');
+      }).toThrowError()
+    });
+
+    it('add agent twice', () => {
+      const domain1 = new Domain();
+      domain1.addAgent(Agent);
+      expect(() => {
+        domain1.addAgent(Agent);
+      }).toThrowError('Duplicated agent type identifier TestAgent2 is not allowed');
+
+    });
+
+    it('create same agent again', () => {
+      const domain1 = new Domain();
+      domain1.addAgent(Agent);
+      const a1 = domain1.getAgent('TestAgent');
+      const a2 = domain1.getAgent('TestAgent2');
+      expect(a1).toEqual(a2);
+      expect(() => {
+        domain1.createAgent(Agent);
+      }).toThrowError('Can not create agent. Duplicated agent identifier TestAgent2 is not allowed');
+    });
+
+    it('add same agent again', () => {
+      const domain1 = new Domain();
+      domain1.createAgent(Agent);
+      expect(() => {
+        domain1.addAgent(Agent);
+      }).toThrowError('Can not add agent type. Duplicated agent type identifier TestAgent2 is not allowed');
+    });
+
+    it('add same agent and new again', () => {
+      const domain1 = new Domain();
+      domain1.createAgent(Agent);
+      expect(() => {
+        const a1 = new Agent(domain1);
+      }).toThrowError('Duplicated agent identifier TestAgent2 is not allowed');
+    });
+
+  });
 
 });
