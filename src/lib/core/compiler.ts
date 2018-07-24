46ba4a3946cf5f274e641489296701614c554176
e2tox <e2tox@live.com>
2018-07-24 11:55:45 +0800

    update typedconstructor

--- src/lib/core/compiler.ts
+++ src/lib/core/compiler.ts
@@ -9,7 +9,6 @@ import { IInvocation } from './invocation';
 import { CONSTRUCTOR_INITIALIZER, FIELD_INITIALIZER } from './symbol';
 import { Reflector } from './reflector';
 
-
 /**
  * AgentCompileType
  */
@@ -41,8 +40,8 @@ export enum AgentFeatures {
  * CompilerOptions
  */
 export interface CompilerOptions {
-  features: AgentFeatures,
-  target: 'function' | 'class' | 'proxy'
+  features: AgentFeatures;
+  target: 'function' | 'class' | 'proxy';
 }
 
 /**
@@ -50,7 +49,6 @@ export interface CompilerOptions {
  * @hidden
  */
 export class Compiler {
-
   _options: Partial<CompilerOptions>;
 
   constructor(options?: Partial<CompilerOptions>) {
@@ -59,20 +57,18 @@ export class Compiler {
     // We will use `Lazy`(on-demand compile) method to upgrade user class into agent
     // please refer to ADR-0004
     options.target = options.target || 'function';
-    options.features = options.features == null ? (AgentFeatures.Initializer ^ AgentFeatures.Interceptor) : options.features;
+    options.features =
+      options.features == null ? AgentFeatures.Initializer ^ AgentFeatures.Interceptor : options.features;
 
     this._options = options;
-
   }
 
   compile(target, parameters?: ArrayLike<any>): Constructor {
-
     let params: Map<number, IInvocation>,
-      fields: PropertyDescriptorMap = {},
+      fields: any = {},
       methods: PropertyDescriptorMap;
 
     if ((this._options.features & AgentFeatures.Initializer) === AgentFeatures.Initializer) {
-
       const lazy = (this._options.features & AgentFeatures.LazyInitializer) === AgentFeatures.LazyInitializer;
 
       // constructor parameter initializer
@@ -87,23 +83,21 @@ export class Compiler {
           // bag.set(key, { value: initializedValue });
           if (lazy) {
             fields[key] = {
-              get: function () {
+              get: function() {
                 const value = (initializer as IInvocation).invoke(parameters);
                 Reflect.defineProperty(this, key, { value });
                 return value;
               }
             };
-          }
-          else {
+          } else {
             const initializedValue = (initializer as IInvocation).invoke(parameters);
             fields[key] = {
               value: initializedValue
             };
           }
-          fields[key][FIELD_INITIALIZER] = initializer // cache initializer
+          fields[key][FIELD_INITIALIZER] = initializer; // cache initializer
         }
       }
-
     }
 
     if ((this._options.features & AgentFeatures.Interceptor) === AgentFeatures.Interceptor) {
@@ -114,24 +108,18 @@ export class Compiler {
     let CompiledAgent;
 
     if (Object.keys(fields).length || methods) {
-
       // EPIC: inject the intercepted value before construct a new instance
       if (this._options.target === 'class') {
-        CompiledAgent = class extends target {
-        };
-      }
-      else {
+        CompiledAgent = class extends target {};
+      } else {
         /* istanbul ignore next */
-        CompiledAgent = function () {
-        };
+        CompiledAgent = function() {};
         CompiledAgent.prototype = Object.create(target.prototype);
       }
 
       fields && Object.defineProperties(CompiledAgent.prototype, fields);
       methods && Object.defineProperties(CompiledAgent.prototype, methods);
-
-    }
-    else {
+    } else {
       CompiledAgent = target;
     }
 
@@ -140,18 +128,15 @@ export class Compiler {
     }
 
     return CompiledAgent;
-
   }
 
   private static makeConstructorParameterInitializers(target): Map<number, IInvocation> {
-
     const method = Reflector(target);
 
     const maxParameter = method.parameterCount();
     let constructorParameterInitializers: Map<number, IInvocation> = new Map<number, IInvocation>();
 
     for (let idx = 0; idx < maxParameter; idx++) {
-
       const parameter = method.parameter(idx);
 
       // get all initializer
@@ -169,38 +154,34 @@ export class Compiler {
       if (intercepted instanceof InitializerInvocation || intercepted instanceof InterceptorInvocation) {
         constructorParameterInitializers.set(idx, intercepted);
       }
-
     }
 
     return constructorParameterInitializers;
   }
 
   private static makePropertyInterceptors(target): PropertyDescriptorMap {
-
     // 1. find all the properties for this type which contains one or more attribute implemented 'getInterceptor'
     const properties: Property[] = Lookup.findInterceptors(target);
     if (!properties.length) {
       return;
     }
 
-    let propertyInterceptors: PropertyDescriptorMap = {};
+    let propertyInterceptors: any = {};
 
     for (const property of properties) {
-
       const name = property.targetKey;
       const descriptor = property.descriptor;
 
       if (!descriptor) {
-
         if (property.hasInitializer() || property.value().hasInitializer()) {
           // Interceptor can works with Initializer
           continue;
+        } else {
+          throw new Error(
+            `Class: ${target.prototype.constructor.name}; Property: ${property.targetKey.toString()}; ` +
+              `Interceptor not work with field property without Initializer`
+          );
         }
-        else {
-          throw new Error(`Class: ${target.prototype.constructor.name}; Property: ${property.targetKey}; ` +
-            `Interceptor not work with field property without Initializer`);
-        }
-
       }
 
       // refer to the origin descriptor
@@ -214,57 +195,71 @@ export class Compiler {
       let interceptorAttributes = property.getInterceptors();
 
       if (value) {
-
         // call interceptors on value first
         // then call interceptors on property
-        interceptorAttributes = property.value().getInterceptors().concat(interceptorAttributes);
+        interceptorAttributes = property
+          .value()
+          .getInterceptors()
+          .concat(interceptorAttributes);
 
         /* istanbul ignore else  */
         if (typeof value === 'function') {
           propertyInterceptors[name].value = InterceptorFactory.createFunctionInterceptor(interceptorAttributes, value);
-        }
-        else {
-          throw new Error(`Class: ${target.prototype.constructor.name}; Property: ${property.targetKey}; ` +
-            `Interceptor not work with non-function property`);
+        } else {
+          throw new Error(
+            `Class: ${target.prototype.constructor.name}; Property: ${property.targetKey.toString()}; ` +
+              `Interceptor not work with non-function property`
+          );
         }
       }
 
       if (typeof getter === 'function') {
-        interceptorAttributes = property.getter().getInterceptors().concat(interceptorAttributes);
-        propertyInterceptors[name].get = InterceptorFactory.createFunctionInterceptor(interceptorAttributes, getter) as () => any;
+        interceptorAttributes = property
+          .getter()
+          .getInterceptors()
+          .concat(interceptorAttributes);
+        propertyInterceptors[name].get = InterceptorFactory.createFunctionInterceptor(
+          interceptorAttributes,
+          getter
+        ) as () => any;
       }
 
       if (typeof setter === 'function') {
-        interceptorAttributes = property.setter().getInterceptors().concat(interceptorAttributes);
-        propertyInterceptors[name].set = InterceptorFactory.createFunctionInterceptor(interceptorAttributes, setter) as (v: any) => void;
+        interceptorAttributes = property
+          .setter()
+          .getInterceptors()
+          .concat(interceptorAttributes);
+        propertyInterceptors[name].set = InterceptorFactory.createFunctionInterceptor(
+          interceptorAttributes,
+          setter
+        ) as (v: any) => void;
       }
-
     }
 
     return propertyInterceptors;
   }
 
   private static makePropertyInitializers(target): Map<string | symbol, IInvocation> {
-
     const initializers: Property[] = Lookup.findInitializers(target);
     let propertyInitializers: Map<string | symbol, IInvocation>;
 
     if (initializers.length > 0) {
-
       propertyInitializers = new Map<string, IInvocation>();
 
       for (const property of initializers) {
-
         const name = property.targetKey;
 
         if (property.descriptor) {
-          throw new Error(`Class: ${target.prototype.constructor.name}; Property: ${property.targetKey}; ` +
-            `Initializer not work with field property`);
-        }
-        else {
-
+          throw new Error(
+            `Class: ${target.prototype.constructor.name}; Property: ${property.targetKey.toString()}; ` +
+              `Initializer not work with field property`
+          );
+        } else {
           let initializerAttributes = property.getInitializers();
-          initializerAttributes = property.value().getInitializers().concat(initializerAttributes);
+          initializerAttributes = property
+            .value()
+            .getInitializers()
+            .concat(initializerAttributes);
 
           // one property may have more than one interceptor.
           // we will call them one by one. passing the result of previous interceptor to the new interceptor
@@ -272,7 +267,10 @@ export class Compiler {
 
           // get all interceptors for the initializer
           let interceptorAttributes = property.getInterceptors();
-          interceptorAttributes = property.value().getInterceptors().concat(interceptorAttributes);
+          interceptorAttributes = property
+            .value()
+            .getInterceptors()
+            .concat(interceptorAttributes);
 
           // apply interceptors
           const intercepted = createInterceptionChainFromAttribute(initialized, interceptorAttributes);
@@ -283,11 +281,9 @@ export class Compiler {
             propertyInitializers.set(name, intercepted);
           }
         }
-
       }
     }
 
     return propertyInitializers;
   }
-
 }
