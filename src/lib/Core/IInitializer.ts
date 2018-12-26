2f0a53efa4da36efe412043e61b24094f0c44576
e2tox <e2tox@live.com>
2018-12-26 17:29:07 +0800

    cleanup code

--- src/lib/Core/IInitializer.ts
+++ src/lib/Core/IInitializer.ts
@@ -4,8 +4,5 @@ import { IInvocation } from './IInvocation';
  * Initialize a field
  */
 export interface IInitializer {
-  configurable?: boolean;
-  enumerable?: boolean;
-  writable?: boolean;
   initialize(target: IInvocation, parameters: ArrayLike<any>): any;
 }
