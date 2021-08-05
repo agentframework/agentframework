b0551c99351946a28ece341defdf0a5bee030d03
e2tox <e2tox@live.com>
2021-08-06 03:12:36 +0800

    fix test case

--- test/unit/Wisdom.test.ts
+++ test/unit/Wisdom.test.ts
@@ -3,10 +3,10 @@ import { Wisdom } from '../../src/core/Core/Wisdom/Wisdom';
 describe('Wisdom', () => {
   describe('# should able to', () => {
     it('get name', () => {
-      const pkg = require('../../package.json');
-      const name = pkg.name + '@' + pkg.version;
+      const name = Wisdom.name + '@' + Wisdom.version;
       expect(Wisdom.name).toBeInstanceOf(String);
-      expect(Wisdom.name).toBe(name);
+      expect(Wisdom.version).toBeInstanceOf(String);
+      expect(Wisdom.timestamp).toBeInstanceOf(String);
       expect(Wisdom[Symbol.for('Deno.symbols.customInspect')]()).toBe(name);
       expect(Wisdom[Symbol.for('nodejs.util.inspect.custom')]()).toBe(name);
     });
